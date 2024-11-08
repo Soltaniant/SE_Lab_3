e001d3db3d3462f20ea46ce805a6e68483d83f4b
mousavi1382 <me.mousavi1382@gmail.com>
2024-11-08 21:34:14 +0330

    complete removeUser function in UserService

--- src/main/java/ir/selab/tdd/service/UserService.java
+++ src/main/java/ir/selab/tdd/service/UserService.java
@@ -37,8 +37,7 @@ public class UserService {
     }
 
     public boolean removeUser(String username) {
-        // TODO: implement
-        return false;
+        return repository.removeUser(username);
     }
 
     public List<User> getAllUsers() {
